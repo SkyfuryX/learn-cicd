f88ba6aaea9b00aa480363f92f7c76e40ca6068c
SkyfuryX <gannonshanley@gmail.com>
2026-07-12 08:12:18 -0400

    committing failed test to test ci

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func Test_api_key_good(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey beeebooops")
+	got, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	want := "beeebooop"
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("want %v, got %v", want, got)
+	}
+}
+
+func Test_api_key_malformed(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey")
+	_, err := GetAPIKey(headers)
+	if err.Error() != "malformed authorization header" {
+		t.Fatalf("want 'malformed authorization header' err, got %v", err)
+	}
+}
+
+func Test_no_auth_header(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Bearer", "Token")
+	_, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("want ErrNoAuthHeaderIncluded err, got %v", err)
+	}
+}
